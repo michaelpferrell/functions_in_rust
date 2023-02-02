0f792d7a4766da3af5b94ca086895873a9085372
Mike Ferrell <mikeferrell.neo@gmail.com>
2023-02-01 21:23:25 -0500

    * Fixed minor naming issues in main.rs of functions_in_rust. * Function try_adding now uses function add_2_ints. * Removed aborted from enum NumberParseResult for now, made others Pascal-cased.

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ fn main() {
 
 fn try_adding() {
     println!("* Adding *");
-    let (a, my_status) = get_int_from_user("Please enter value A.");
+    let (a, my_status) = try_getting_int_from_user_once("Please enter value A.");
     println!("Entered for A: {}", a);
-    let (b, my_status) = get_int_from_user("Please enter value B.");
+    let (b, my_status) = try_getting_int_from_user_once("Please enter value B.");
     println!("Entered for B: {}", b);
-    let my_sum = a+b;
+    let my_sum = add_2_ints(a, b);
     println!("{} + {} = {}", a, b, my_sum);
 }
 
-fn get_int_from_user(prompt: &str) -> (i32, NumberParseResult) {
+fn try_getting_int_from_user_once(prompt: &str) -> (i32, NumberParseResult) {
     let mut my_input = String::new();
 
     println!("{}", prompt);
@@ -24,13 +24,13 @@ fn get_int_from_user(prompt: &str) -> (i32, NumberParseResult) {
         .read_line(&mut my_input)
         .expect("Failed to read line");
 
-    let mut my_status: NumberParseResult = NumberParseResult::ok;
+    let mut my_status: NumberParseResult = NumberParseResult::Ok;
     let my_num: i32 = match my_input.trim().parse()
     {
         Ok(num) => num,
         Err(_) => {
             println!("A number is required!");
-            my_status = NumberParseResult::err;
+            my_status = NumberParseResult::Err;
             999
         },
     };
@@ -42,7 +42,6 @@ fn add_2_ints(a: i32, b: i32) -> i32 {
 }
 
 enum NumberParseResult {
-    ok,
-    err,
-    aborted
+    Ok,
+    Err
 }
